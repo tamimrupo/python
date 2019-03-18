44a53843481b60cb302bcb42b2646af4e6ac4664
Tamim Rupo <36479646+tamimrupo@users.noreply.github.com>
2019-03-18 17:24:22 +0600

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,14 @@
+is_hot = True
+is_clod = False
+if is_hot:
+    print("It's a hot day")
+    print("Drink plenty of water")
+
+elif is_clod:
+    print("It's a clod day")
+    print("Wear warm clothes")
+
+else:
+    print("It's a lovely day")
+
+print("Enjoy your day")
